3a6593cd9a86a82bc4018907ad646f419fc4f887
slawek pelka <slawek.pelka@indicia.nl>
2016-02-11 21:22:15 +0100

    html base

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
-$(function() {
-  //
+'use strict';
+
+var todo = {} || '';
+
+todo = {
+
+	init: function() {
+		var pageTitle = $(".title");
+		var listTitle = $(".curr-list-title");
+
+		todo.loadDash();
+		todo.switcher();
+	},
+
+	switcher: function() {
+
+		$(".buttons button").on("click", function(e) {
+			e.preventDefault();
+			var place = $(this).attr("place");
+
+			switch(place) {
+				case "dashboard":
+					todo.loadDash();
+					break;
+				case "todo":
+					todo.loadTodo();
+					break;
+				case "completed":
+					todo.loadCompleted();
+					break;
+			}
+		});
+
+	},
+
+	loadDash: function() {
+		$("");
+	},
+
+	loadTodo: function() {
+
+	},
+
+	loadCompleted: function() {
+
+	}
+
+
+};
+
+$(document).ready( function() {
+	todo.init();
 });
