186e41470bc595ca66a2f98f66f2ee0e3d36f33b
quincy-soeliman <qsoeliman.dev@gmail.com>
2016-02-11 21:41:56 +0100

    Added some styling and fonts

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>ToDo App</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<div class="header">
+  <div class="container">
+    <div class="row">
+      <div class="logo">
+        <?php print file_get_contents('images/task.svg'); ?>
+      </div>
+
+      <div class="site-title">
+        <h2>ToDo Application</h2>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="main-content container">
+  <!-- -->
+</div>
+
+<div class="footer">
+  <!-- -->
+</div>
+
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
